587f8cd3b0fd1fc5d52a2ca656ae62221b0e82c7
kmecseki <kmecseki@gmail.com>
2024-07-03 18:44:43 -0700

    Fixed small bugs and added documentation.

---
+++ analyze.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import json
+import os
+
+def top10_most_traded():
+    lista = []
+    for fname in os.listdir("./dump/"):
+        if fname.endswith(".json"):
+            filename = os.path.join("./dump", fname)
+            with open(filename,'r') as f: # check extension?
+                data = json.load(f)
+        item = []
+        item.append(fname.removesuffix(".json"))
+        # Reported trades - long term
+        longterm_trades_data = data['payload']['statistics_closed']['90days']
+        if longterm_trades_data != []:
+            df = pd.DataFrame(longterm_trades_data)
+            # Fix time
+            df['time'] = pd.to_datetime(df['datetime'])
+            # Only rank 0
+            if 'mod_rank' in df.columns:
+                rank0 = df[df['mod_rank'] == 0]
+            else:
+                rank0 = df
+            total_volume = rank0['volume'].sum()
+            item.append(total_volume)
+            avg_price = rank0['avg_price'].mean()
+            item.append(avg_price)
+            lista.append(item)
+    return lista
+
+lista = top10_most_traded()
+df2 = pd.DataFrame(lista)
+df2.columns = ['name', 'volume', 'avg_price']
+top10 = df2.sort_values(by='volume', ascending=False).head(50)
+print(top10)
+
+
+
+
+
--- wgraph.py
+++ wgraph.py
@@ -1,1 +1,61 @@
-a
+"""
+Functions to download data to analyze using the wf market API.
+Saves data as a file.
+"""
+
+import os
+import json
+import time
+import requests
+
+def getitems(tag):
+    """Get current item list."""
+
+    items = []
+    for filename in os.listdir("./items/wfm-items/tracked/items/"):
+        with open(os.path.join("./items/wfm-items/tracked/items/"\
+                               ,filename), 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            if "tags" in data and tag == "arcanes":
+                if "arcane_enhancement" in data["tags"]:
+                    make_url = url_gen(data)
+                    items.append(make_url)
+                    #items.append(data.get('i18n', {}).get('en', {}).get('item_name'))
+            if "tags" in data and tag == "mods":
+                if "mod" in data["tags"] and "warframe" in data["tags"]:
+                    make_url = url_gen(data)
+                    items.append(make_url)
+    return items
+
+def url_gen(data):
+    """Generates url for the API request."""
+
+    pair = []
+    name = data.get('url_name', {})
+    filename = os.path.join("./dump/",str(data.get('i18n', {}).get('en', {})\
+                                          .get('item_name')) + ".json")
+    pair.append(filename)
+    entire_url = "https://api.warframe.market/v1/items/"\
+         + str(name) + "/statistics"
+    pair.append(entire_url)
+    return pair
+
+def fetch_json_and_save(url):
+    """Fetches json data and saves it as a file."""
+
+    try:
+        response = requests.get(url[1], timeout=50)
+        response.raise_for_status()  # Check for HTTP errors
+        json_data = response.json()  # Parse the JSON data from the response
+        with open(url[0], 'w', encoding='utf-8') as file:
+            json.dump(json_data, file, ensure_ascii=False, indent=4)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while fetching the JSON data: {e}")
+    except Exception as e:
+        print(f"An error occurred while saving the JSON data: {e}")
+
+lista = getitems("mods") # "arcanes", "mods", "component", "weapon", "prime", "relic"
+for urls in lista:
+    fetch_json_and_save(urls)
+    time.sleep(3)
+    print(f"Processing {urls[0]}")
